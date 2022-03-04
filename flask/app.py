5e2191671c7e2da1d5514324b62998db4419ff7c
joyeuse-cloud <bizimanafabrice707@gmail.com>
2022-03-04 15:56:43 +0200

    edition

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+import http
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "<h1>Hello world</h1"
+def index():
+    return render_template('home.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/book/<str:id>')
+def book(id):
+    return render_template('book.html')
+
+@app.route('/update/<str:id>')
+def update(id):
+    return render_template('book.html')
+
+@app.errorhandler(404)
+def page404(error):
+    return render_template('page404.html')
