8d2fcab7d3b5b961d1719a2cc9dca03a31327665
joyeuse-cloud <bizimanafabrice707@gmail.com>
2022-03-04 16:51:16 +0200

    book app formed

--- flask/app.py
+++ flask/app.py
@@ -1,3 +1,4 @@
+from distutils.debug import DEBUG
 import http
 from flask import Flask, render_template, url_for
 
@@ -11,14 +12,17 @@ def index():
 def add():
     return render_template('add.html')
 
-@app.route('/book/<str:id>')
+@app.route('/book/<int:id>')
 def book(id):
     return render_template('book.html')
 
-@app.route('/update/<str:id>')
+@app.route('/update/<int:id>')
 def update(id):
-    return render_template('book.html')
+    return render_template('update.html')
 
 @app.errorhandler(404)
 def page404(error):
     return render_template('page404.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
